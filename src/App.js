29e119fd4d7a25cabb63a3d577b8275be261ade7
nima babaei <nimani1378@gmail.com>
2022-10-03 15:16:09 +0330

    add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
-import router from "./routes/routes";
+import Home from "./pages/home/index.jsx";
+import Add_contact from "./pages/add-contact/add_contact.jsx"
 import { RouterProvider } from "react-router-dom";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: "/addContact",
+      element: <Add_contact/>,
+    },
+    // {
+    //   path: "/",
+    //   element: <Layout2 />,
+    //   children: [
+    //     {
+    //       path: "login",
+    //       element: <Login />,
+    //     },
+    //     {
+    //       path: "register",
+    //       element: <Register />,
+    //     },
+    //   ],
+    // },
+  ]);
   return <RouterProvider router={router} />;
 }
 
